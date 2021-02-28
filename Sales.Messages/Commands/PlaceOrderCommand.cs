73070b2f7123408222ee44c83a9c0276b841d829
Pablo <p.c.ramirez5150@gmail.com>
2021-02-28 17:11:28 +0100

    Initial commit

---
+++ Sales.Messages/Commands/PlaceOrderCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sales.Messages.Commands
+{
+    public class PlaceOrderCommand
+    {
+        public string UserId { get; set; }
+        public string[] ProductIds { get; set; }
+        public string ShippingTypeId { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
