8f85e9ceef0a1b2305436d0423a4691be858b8b2
Pablo Company Ramírez <p.c.ramirez5150@gmail.com>
2021-03-03 23:01:50 +0100

    Tests MessageBus dependency injection

---
+++ DDDesign.Web/IMessageBus.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+namespace DDDesign.Web
+{
+    public interface IMessageBus
+    {
+        Task Send(IMessage message);
+    }
+}
---
+++ DDDesign.Web/MessageBus.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+
+namespace DDDesign.Web
+{
+    public class MessageBus : IMessageBus
+    {
+        private readonly IEndpointInstance endpointInstance;
+        
+        public MessageBus()
+        {
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+            endpointConfiguration.UseTransport<LearningTransport>();
+
+            endpointInstance = Endpoint.Start(endpointConfiguration)
+                .ConfigureAwait(false).GetAwaiter().GetResult();    
+        }
+
+        public Task Send(IMessage message)
+        {
+            return endpointInstance.Send(message);
+        }
+    }
+}
