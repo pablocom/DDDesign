44feda8076811e2f786993f538fd3795e724863d
Pablo <p.c.ramirez5150@gmail.com>
2021-03-04 19:18:00 +0100

    feat(Web): Configure synchronization context to to false

--- DDDesign.Web/Startup.cs
+++ DDDesign.Web/Startup.cs
@@ -23,7 +23,7 @@ namespace DDDesign.Web
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
-            services.AddScoped<IMessageBus, MessageBus>();
+            services.AddSingleton<IMessageBus, MessageBus>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
